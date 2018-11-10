9670812be1837013a80f4e0597b5b87973850815
mnindrazaka <mnindrazaka@gmail.com>
2018-11-10 10:52:27 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentTime: null
+    }
+    setInterval(() => this.changeTime(), 1000)
+  }
+
+  changeTime() {
+    const currentTime = new Date().toLocaleTimeString()
+    this.setState({ currentTime })
+  }
+
   render() {
+    const currentTime = new Date()
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {currentTime.toLocaleTimeString()}
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
